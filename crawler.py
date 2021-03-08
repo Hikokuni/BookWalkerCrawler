b33f123c25c438b4de83995f1775d177479a7abb
Ryuu Hikokuni <albert19920831@gmail.com>
2021-03-07 22:47:51 -0800

    Additional command line optional arguments

--- crawler.py
+++ crawler.py
@@ -8,15 +8,18 @@ import time, util, base64, os, sys, argparse
 
 # Setup parser
 parser = argparse.ArgumentParser(description='对BookWalker用爬虫（伪）')
-parser.add_argument('Username', metavar='username', type=str, help='BookWalker Username')
-parser.add_argument('Password', metavar='password', type=str, help='BookWalker Password')
+parser.add_argument('Username', metavar='username', type=str, help='BookWalker 用户名')
+parser.add_argument('Password', metavar='password', type=str, help='BookWalker 密码')
+parser.add_argument('-w', '--wait', action='store', type=float, default=2, help='Web Driver等待时间（可近似看做翻页时间），默认为2秒，网络不好可适当增加')
+parser.add_argument('-l', '--load', action='store', type=float, default=16, help='图书最初加载时间，默认为16秒，网络不好可适当增加')
+parser.add_argument('-p', '--path', action='store', type=str, default='Download', help='图片存储路径，默认为项目文件夹下的Download文件夹')
 args = parser.parse_args()
 
 # Setup driver
 options = webdriver.ChromeOptions() 
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(2)
+driver.implicitly_wait(args.wait)
 driver.get("https://global.bookwalker.jp/")
 
 # Sign in and navigate to books page
@@ -32,20 +35,22 @@ books = driver.find_elements_by_xpath("//section[contains(@class, 'book-item')]/
 i = 1
 for book in books:
     print('{}. {}'.format(i, book.text))
-selection = int(input('Select the book to scrape: '))
+    i += 1
+selection = int(input('选择图书序号：'))
 
 # Open book
 util.select_with_timeout(driver, "(//section[contains(@class, 'book-item')]//span[text()='Read this book'])[{}]".format(selection)).click()
 driver.switch_to.window(driver.window_handles[-1])
 
-# Deal with pecial case (first page)
-time.sleep(30)
-Path('Download/').mkdir(parents=True, exist_ok=True)
+# Deal with special case (first page)
+time.sleep(args.load)
+download_path = args.path
+Path(download_path).mkdir(parents=True, exist_ok=True)
 i = 1
 canvas = util.select_with_timeout(driver, "//div[@id='viewport1']/canvas")
 canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
 canvas_png = base64.b64decode(canvas_base64)
-with open(r'Download/page_{}.png'.format(i), 'wb') as f:
+with open(r'{}/page_{}.png'.format(download_path, i), 'wb') as f:
     f.write(canvas_png)
 i += 1
 util.send_keys(driver, Keys.LEFT)
@@ -56,7 +61,7 @@ while(not util.element_present(driver, "//div[@id='dialog']/div[@style='display:
     canvas = util.select_with_timeout(driver, "//div[@id='viewport0']/canvas")
     canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
     canvas_png = base64.b64decode(canvas_base64)
-    with open(r'Download/page_{}.png'.format(i), 'wb') as f:
+    with open(r'{}/page_{}.png'.format(download_path, i), 'wb') as f:
         f.write(canvas_png)
     i += 1
 
@@ -64,7 +69,7 @@ while(not util.element_present(driver, "//div[@id='dialog']/div[@style='display:
     canvas = util.select_with_timeout(driver, "//div[@id='viewport1']/canvas")
     canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);", canvas)
     canvas_png = base64.b64decode(canvas_base64)
-    with open(r'Download/page_{}.png'.format(i), 'wb') as f:
+    with open(r'{}/page_{}.png'.format(download_path, i), 'wb') as f:
         f.write(canvas_png)
     i += 1
 
